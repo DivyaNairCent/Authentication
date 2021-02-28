6257e580d0ecdc123d1cb9b9c94731e2e8e44e8d
DivyaNairCent <dnair8@my.centennialcollege.ca>
2021-03-01 04:13:03 +0530

    db changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/business_contacts"
+    //"URI": "mongodb://localhost/business_contacts"
+    "URI" : "mongodb+srv://divya:LyFBAeqlWAH7xBDT@mongodbserver.kjwox.mongodb.net/business_contacts?retryWrites=true&w=majority"
 }
